a98bab00f06c10be17f0975dddfc7eab5b4449de
Andrew Ochsner <aochsner@cs.stanford.edu>
2019-01-31 16:22:17 -0600

    ensure each step succeeds when create/deleting

--- create-standard-riglet.sh
+++ create-standard-riglet.sh
@@ -4,10 +4,10 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 function createEnvironment() {
-  echo "Y" | make create-foundation ENV=${1}
-  echo "Y" | make create-compute ENV=${1}
-  echo "Y" | make create-db ENV=${1}
-  echo "Y" | make create-app-deps ENV=${1}
+  echo "Y" | make create-foundation ENV=${1} &&
+  echo "Y" | make create-compute ENV=${1} &&
+  echo "Y" | make create-db ENV=${1} &&
+  echo "Y" | make create-app-deps ENV=${1} &&
   echo "Y" | make upload-app ENV=${1}
 }
 
@@ -18,10 +18,10 @@ read -p "Are you sure you want to proceed?  " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  make create-deps
-  createEnvironment integration
-  createEnvironment staging
-  createEnvironment production
-  # echo "Y" | make create-build REPO=bookit-api CONTAINER_PORT=8080 CONTAINER_MEMORY=512 LISTENER_RULE_PRIORITY=300
+  make create-deps &&
+  createEnvironment integration &&
+  createEnvironment staging &&
+  createEnvironment production &&
+  # echo "Y" | make create-build REPO=bookit-api CONTAINER_PORT=8080 CONTAINER_MEMORY=512 LISTENER_RULE_PRIORITY=300 &&
   # echo "Y" | make create-build REPO=bookit-client-react SUBDOMAIN=bookit CONTAINER_PORT=4200 CONTAINER_MEMORY=128 LISTENER_RULE_PRIORITY=200
 fi
--- delete-standard-riglet.sh
+++ delete-standard-riglet.sh
@@ -6,16 +6,16 @@ NC='\033[0m' # No Color
 # Each delete target asks a Y/N question, so we can't just do make delete-environment
 # to do unattended deletions, unfortunately
 function deleteEnvironment() {
-  echo "Y" | make delete-db ENV=${1}
-  echo "Y" | make delete-compute ENV=${1}
-  echo "Y" | make delete-foundation ENV=${1}
+  echo "Y" | make delete-db ENV=${1} &&
+  echo "Y" | make delete-compute ENV=${1} &&
+  echo "Y" | make delete-foundation ENV=${1} &&
   echo "Y" | make delete-app-deps ENV=${1}
 }
 
 function deleteAppsAndBuild() {
-  echo "Y" | make delete-app ENV=integration REPO=${1}
-  echo "Y" | make delete-app ENV=staging REPO=${1}
-  echo "Y" | make delete-app ENV=production REPO=${1}
+  echo "Y" | make delete-app ENV=integration REPO=${1} &&
+  echo "Y" | make delete-app ENV=staging REPO=${1} &&
+  echo "Y" | make delete-app ENV=production REPO=${1} &&
   echo "Y" | make delete-build REPO=${1}
 }
 
@@ -41,10 +41,10 @@ read -p "Are you ABSOLUTELY SURE you want to proceed?  " -n 1 -r -t 3
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # deleteAppsAndBuild bookit-client-react
-  # deleteAppsAndBuild bookit-api
-  deleteEnvironment integration
-  deleteEnvironment staging
-  deleteEnvironment production
+  # deleteAppsAndBuild bookit-client-react &&
+  # deleteAppsAndBuild bookit-api &&
+  deleteEnvironment integration &&
+  deleteEnvironment staging &&
+  deleteEnvironment production &&
   echo "Y" | make delete-deps
 fi
