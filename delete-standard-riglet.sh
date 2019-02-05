4d34a5516103071dddd79a393b94cec0e9ac04e1
Andrew Ochsner <andrew.ochsner@wipro.com>
2019-02-05 16:25:37 -0600

    minor updates to scripts
    mostly adding `\` to the end of `... && \`
    Also updating building .make to be up to date

--- delete-standard-riglet.sh
+++ delete-standard-riglet.sh
@@ -3,20 +3,22 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+BRANCH=${1:-master}
+
 # Each delete target asks a Y/N question, so we can't just do make delete-environment
 # to do unattended deletions, unfortunately
 function deleteEnvironment() {
-  echo "Y" | make delete-db ENV=${1} &&
-  echo "Y" | make delete-compute ENV=${1} &&
-  echo "Y" | make delete-foundation ENV=${1} &&
+  echo "Y" | make delete-db ENV=${1} && \
+  echo "Y" | make delete-compute ENV=${1} && \
+  echo "Y" | make delete-foundation ENV=${1} && \
   echo "Y" | make delete-app-deps ENV=${1}
 }
 
 function deleteAppsAndBuild() {
-  echo "Y" | make delete-app ENV=integration REPO=${1} &&
-  echo "Y" | make delete-app ENV=staging REPO=${1} &&
-  echo "Y" | make delete-app ENV=production REPO=${1} &&
-  echo "Y" | make delete-build REPO=${1}
+  echo "Y" | make delete-app ENV=integration REPO=${1} REPO_BRANCH=${BRANCH} && \
+  echo "Y" | make delete-app ENV=staging REPO=${1} REPO_BRANCH=${BRANCH} && \
+  echo "Y" | make delete-app ENV=production REPO=${1} REPO_BRANCH=${BRANCH} && \
+  echo "Y" | make delete-build REPO=${1} REPO_BRANCH=${BRANCH}
 }
 
 cat .make
@@ -41,10 +43,10 @@ read -p "Are you ABSOLUTELY SURE you want to proceed?  " -n 1 -r -t 3
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # deleteAppsAndBuild bookit-client-react &&
-  # deleteAppsAndBuild bookit-api &&
-  deleteEnvironment integration &&
-  deleteEnvironment staging &&
-  deleteEnvironment production &&
+  # deleteAppsAndBuild bookit-client-react && \
+  # deleteAppsAndBuild bookit-api && \
+  deleteEnvironment integration && \
+  deleteEnvironment staging && \
+  deleteEnvironment production && \
   echo "Y" | make delete-deps
 fi
--- scripts/build-dotmake.sh
+++ scripts/build-dotmake.sh
@@ -3,26 +3,19 @@
 echo 'Please fill in the config settings to store in your .make.'
 echo 'Defaults are shown in parens.  <Enter> to accept.'
 echo
-read -p 'Domain ("buildit.tools"): ' domain
-read -p 'AWS SSH keyname: ' keyname
 read -p 'Owner of riglet: ' owner
-read -p 'AWS Profile ("default"): ' profile
 read -p 'Project ("reference"): ' project
 read -p 'AWS region ("us-east-1"): ' region
-read -p 'AWS Certificate Manager GUID (NO DEFAULT.  Use buildit.tools cert GUID for your region.): ' domain_guid
+read -p 'AWS Profile ("default"): ' profile
 echo
 
 cat << EOF > .make
-DOMAIN = ${domain:-buildit.tools}
-KEY_NAME = ${keyname}
 OWNER = ${owner}
-PROFILE = ${profile:-default}
 PROJECT = ${project:-reference}
+PROFILE = ${profile:-default}
 REGION = ${region:-us-east-1}
-DOMAIN_CERT = ${domain_guid}
 EOF
 
 echo 'Saved .make!'
 echo 'Please verify with "make check-env"!'
 echo
-
